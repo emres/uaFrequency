db5df9aa41ae290c943817e7d97e1d4d64396fe8
Emre Sevinc <emre.sevinc@gmail.com>
2010-05-10 16:50:33 +0200

    Start and end time echo features added.

--- repeatTest.bat
+++ repeatTest.bat
@@ -1,3 +1,15 @@
+@echo off
+
+FOR /F "tokens=*" %%i IN ('time /T') DO SET startTime=%%i 
+
+echo Start time : %startTime%
+
 rm frequency.db
 python createDB.py
 python convert.py
+
+FOR /F "tokens=*" %%i IN ('time /T') DO SET endTime=%%i 
+
+echo Start time : %startTime%
+echo End time   : %endTime%
+
