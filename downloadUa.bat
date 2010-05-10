19daa2fb7e402c5df8db9ef8f0617c7489775cb0
Emre Sevinc <emre.sevinc@gmail.com>
2010-05-10 13:33:26 +0200

    10 GB quota added for controlled disk space consumption.

--- downloadUa.bat
+++ downloadUa.bat
@@ -1,1 +1,1 @@
-wget -t 5 -T 10 -r -H -D www.ua.ac.be  http://www.ua.ac.be/main.aspx?c=*UA  -R gif,jpg,jpeg,png,mp3,wav,mpg,mpeg,doc,ppt,xls,docx,js,css,bmp,JPG,swf,ico,Download.aspx,download.aspx
+wget -t 5 -T 10 -r -H -D www.ua.ac.be  -R gif,jpg,jpeg,png,mp3,wav,mpg,mpeg,doc,ppt,xls,docx,js,css,bmp,JPG,swf,ico,Download.aspx,download.aspx -Q 1048576m http://www.ua.ac.be/main.aspx?c=*UA 
