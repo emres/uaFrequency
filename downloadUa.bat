24e391c3cdff336bd379ba52319068a6dac5624c
Emre Sevinc <emre.sevinc@gmail.com>
2010-05-11 15:30:39 +0200

    Added mov and MOV file extensions to the excluded file types list.

--- downloadUa.bat
+++ downloadUa.bat
@@ -1,1 +1,1 @@
-wget -t 2 -T 10 -r -H -D www.ua.ac.be  -R gif,GIF,jpg,jpeg,png,mp3,wav,mpg,mpeg,doc,ppt,xls,docx,pdf,PDF,js,css,bmp,JPG,swf,ico,Download.aspx,download.aspx -Q 1048576m http://www.ua.ac.be/main.aspx?c=*UA 
+wget -t 2 -T 10 -r -H -D www.ua.ac.be  -R gif,GIF,jpg,jpeg,png,mp3,wav,mpg,mpeg,doc,ppt,xls,docx,pdf,PDF,js,css,bmp,JPG,swf,ico,mov,MOV,Download.aspx,download.aspx -Q 1048576m http://www.ua.ac.be/main.aspx?c=*UA 
