bb0cd87c4f80c8065b3dccb90b1f609574d79de5
Emre Sevinc <emre.sevinc@gmail.com>
2010-05-15 10:47:13 +0200

    Normalized the database structure so that urls are stored in the wordReverseIndex table as integer ids. This led to more than %60 reduction in overall database size.

--- createDB.py
+++ createDB.py
@@ -4,8 +4,8 @@ conn = sqlite3.connect('frequency.db')
 c = conn.cursor()
 
 c.execute('CREATE TABLE mainFrequency (word TEXT PRIMARY KEY, frequency INTEGER)')
-c.execute('CREATE TABLE wordReverseIndex (wordReverseIndexID INTEGER PRIMARY KEY AUTOINCREMENT, word TEXT, url TEXT, context TEXT)')
-
+c.execute('CREATE TABLE urlTable (id INTEGER PRIMARY KEY AUTOINCREMENT, url TEXT)')
+c.execute('CREATE TABLE wordReverseIndex (wordReverseIndexID INTEGER PRIMARY KEY AUTOINCREMENT, word TEXT, url INTEGER, context TEXT)')
 
 c.execute('CREATE UNIQUE INDEX idx_mainFrequency ON mainFrequency (word)')
 
